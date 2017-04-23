272aa5e8522623d1f84741365eabee0525563d0a
Francesco Sacchi <batt@develer.com>
2017-04-23 17:21:41 +0200

    Set test times; add load command; shuffle all cards.

--- scripts/almanaccheler.js
+++ scripts/almanaccheler.js
@@ -11,21 +11,22 @@
 
 module.exports = function (robot) {
 
-  var list_url = "https://dl.dropboxusercontent.com/s/7ebi7gcuvc98loy/list.txt"
   var room = "fernandello_test"
 
   var CronJob = require('cron').CronJob;
-  var job = new CronJob('00 41 16 * * *', function() {
-    robot.http(list_url).get()(function(err, resp, body) {
-        if (err) {
-          robot.messageRoom(room, err);
-        }
-        else {
-          var img = body.split('\n');
-          var rnd = Math.floor(Math.random() * img.length);
-          robot.messageRoom(room, img[rnd]);
-        }
-      });
+  var job = new CronJob('0,15,30,45 * * 1-5', function() {
+      var img_list = robot.brain.get('img_list') || null;
+      if (img_list === null)
+        return;
+
+      var idx = robot.brain.get('img_idx');
+      var max_idx= robot.brain.get('img_cnt');
+
+      robot.messageRoom(room, img_list[idx]);
+      idx++;
+      if (idx >= max_idx)
+        idx = 0;
+      robot.brain.set('img_idx', idx);
     }, function () {
       /* This function is executed when the job stops */
     },
@@ -34,8 +35,18 @@ module.exports = function (robot) {
   );
   job.start();
 
+  var shuffle = function(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+    return a;
+  }
 
-  robot.respond(/http (.*)/i, function (res) {
+  robot.respond(/load (.*)/i, function (res) {
     var url = res.match[1].trim()
     robot.http(url)
       .get()(function(err, resp, body) {
@@ -43,7 +54,12 @@ module.exports = function (robot) {
           res.reply(err);
         }
         else
-          res.reply(body);
+          var img = body.split('\n');
+          img = shuffle(img);
+          robot.brain.set('img_list', img);
+          robot.brain.set('img_idx', 0);
+          robot.brain.set('img_cnt', img.length);
+          res.reply("Caricate " + img.length + " immagini:\n" + img.join('\n'));
       });
   });
 
